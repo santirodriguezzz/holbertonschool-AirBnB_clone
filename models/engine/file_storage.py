e483f4590a1d95e1ae51a8891b6bef02db6ed0c9
Denisse Landau <denisselandau97@gmail.com>
2022-10-13 15:20:42 -0300

    task 5;

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,14 +13,14 @@ class FileStorage():
     def all(self):
         """returns dictionary of __objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """
         This function creates a new object of class BaseModel and adds it to the dictionary __objects.
-        
+
         :param obj: the object to be stored
         """
-        
+
         key = f'{obj.__class__.__name__}.{obj.id}'
         self.__objects[key] = obj
 
@@ -39,4 +39,5 @@ class FileStorage():
             with open(self.__file_path, mode='r') as fileJr:
                 loaded_dictionary = json.loads(fileJr.read())
             for values in loaded_dictionary.values():
-                
+                execsClass = values['__class__']
+                self.new(eval(execsClass)(**values))
