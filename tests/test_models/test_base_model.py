a50ba2e1cb8e01636f43187cc42b4d5a722a5105
Denisse Landau <denisselandau97@gmail.com>
2022-10-13 16:34:47 -0300

    try all in one

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,14 +17,5 @@ class Base_Model_Tests(unittest.TestCase):
         self.assertEqual(type(base0.created_at), datetime)
         self.assertEqual(type(base0.updated_at), datetime)
 
-class File_Storage_Tests(unittest.TestCase):
-    """tests file storage file"""
-
-    def test0(self):
-        """tests file storage methods"""
-        file1 = FileStorage()
-
-        self.assertEqual(file1.all(), {})
-
 if __name__ == '__main__':
     unittest.main()
