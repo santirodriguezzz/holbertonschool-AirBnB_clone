bb0382ec9a8b03722900e87858a70a7de10a4958
Denisse Landau <denisselandau97@gmail.com>
2022-10-13 16:41:55 -0300

    please

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models import storage
 import datetime
 
 
@@ -16,6 +17,15 @@ class Base_Model_Tests(unittest.TestCase):
         self.assertEqual(type(base0.id), str, [])
         self.assertEqual(type(base0.created_at), datetime)
         self.assertEqual(type(base0.updated_at), datetime)
+
+class File_Storage_Tests(unittest.TestCase):
+    """tests file storage file"""
+
+    def test0(self):
+        """tests file storage methods"""
+        file1 = FileStorage()
+        file1_list = storage.all()
+        self.assertEqual(type(file1_list), dict)
 
 if __name__ == '__main__':
     unittest.main()
