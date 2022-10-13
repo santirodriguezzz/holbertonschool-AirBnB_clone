1276e1dc3fc04de04fac44e2840d63d71c1ac44d
Denisse Landau <denisselandau97@gmail.com>
2022-10-13 15:53:37 -0300

    try2

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -16,7 +16,7 @@ class Base_Model_Tests(unittest.TestCase):
         self.assertEqual(type(base0.created_at), datetime)
         self.assertEqual(type(base0.updated_at), datetime)
 
-    def file_storage(Self):
+    def file_storage(self):
         """tests file storage methods"""
         file1 = FileStorage()
 
