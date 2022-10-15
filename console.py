0f0249014724460882b9e6a76ad078091233fb1a
santirodriguezzz <santiagorodri130898@gmail.com>
2022-10-14 21:54:52 -0300

    create task 7

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """Module Console"""
 import cmd
 import models
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,9 +18,25 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program"""
         exit()   
 
-    def do_EOF(self,arg):
+    def do_EOF(self, arg):
         """EOF command to exit the program"""
         exit()
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if len(arg) == 0:
+            print('** class name missing**')
+        else:
+            try:
+                newInstance = eval(arg)()
+                newInstance.save()
+                print(newInstance. id)
+            except:
+                print('** class doesnt exist**')
+
+    
+    def do_show(self, arg):
+        """ """
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
