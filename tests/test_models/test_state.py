39dad393aab34b5f8456ab1aef7fef29030164dc
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 19:46:33 -0300

    tests states attr

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,17 @@
+"""python interpreter"""
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models.state import State
+
+
+class State_Tests(unittest.TestCase):
+    """tests attributes from state class"""
+
+    def test_attributes(self):
+        newState = State()
+        self.assertEquals(newState.__class.__name__, "State")
+
+    def test_1(self):
+        newState = User()
+        self.assertEqual(type(newState.name), str)
