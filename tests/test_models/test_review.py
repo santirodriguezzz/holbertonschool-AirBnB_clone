530860f5c5eb86b9c0b7fd14550e138fd30fdce9
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 20:02:12 -0300

    test reviews feature

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""python interpreter"""
+import unittest
+from models.review import Review
+
+
+class Review_Tests(unittest.TestCase):
+    """tests attributes from Review class"""
+
+    def test_attributes(self):
+        newReview = Review()
+        self.assertEquals(newReview.__class.__name__, "Review")
+
+    def test_1(self):
+        newReview = Review()
+        self.assertEqual(type(newReview.place_id), str)
+        self.assertEqual(type(newReview.user_id), str)
+        self.assertEqual(type(newReview.text), str)
