ea5332f8aac78b79bf71cce824e8001d7b7e6265
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 18:18:40 -0300

    import update

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""testing"""
+import unittest
+from models import storage
+import models
+from models.user import User
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class User_Test(unittest.TestCase):
+    """tests user"""
+    pass
