222bd2eea40b5a72ad05701ccbeb9f664d587013
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 18:51:32 -0300

    testing class user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -9,4 +9,14 @@ from models.engine.file_storage import FileStorage
 
 class User_Test(unittest.TestCase):
     """tests user"""
-    pass
+
+    def test_0(self):
+        newUser = User()
+        self.assertEquals(newUser.__class__.__name__, "User")
+
+    def test_1(self):
+        newUser = User()
+        self.assertEqual(type(newUser.email), str)
+        self.assertEqual(type(newUser.password), str)
+        self.assertEqual(type(newUser.first_name), str)
+        self.assertEqual(type(newUser.last_name), str)
