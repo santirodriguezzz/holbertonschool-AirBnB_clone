292eb9bdfa80c03b9c62615bebfe5d20f1d98d14
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 22:20:52 -0300

    fixed added import os

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """python interpreter"""
 import unittest
+import os
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
