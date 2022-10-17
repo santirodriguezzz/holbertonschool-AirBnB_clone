1de4069f99e9f6c4fee1efaefe72f19ce2d6ad80
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 22:14:34 -0300

    tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,23 +12,12 @@ class File_Storage_Tests(unittest.TestCase):
 
     def test__FilePath(self):
         """tests attr file_path"""
-        self.assertEqual(str, type(FileStorage._FileStorage__file_path))
 
-    def test__Obj(self):
-        """tests attr obj"""
-        self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+    def test_filestorage(self):
+        storage = FileStorage()
+        storage.all().clear()
+        storage.reload()
+        self.assertFalse(len(storage.all()) == 0)
 
-    def test_all(self):
-        """tests method all"""
-        dictionary = storage.all()
-        self.assertEqual(type(dictionary), dict)
-
-    def test_new(self):
-        """tests method new"""
-        newobj = storage.all().copy()
-        storage.new(BaseModel())
-        self.assertNotEqual(newobj, storage.all())
-
-    def test_reload(self):
-        """tests reload"""
-        self.assertRaises(FileNotFoundError, models.storage.reload())
+if __name__ == "__main__":
+    unittest.main()
