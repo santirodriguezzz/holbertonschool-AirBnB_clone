6ecfb8799d95f39d5458ce4a196dd89656b938b6
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 22:19:07 -0300

    update test file

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,26 +10,28 @@ import datetime
 class Base_Model_Tests(unittest.TestCase):
     """test attrubute """
 
-    def setUp(self):
-        """set up new basemodel"""
-        self.base = BaseModel()
-
-    def test1(self):
-        """test 1"""
-        self.assertEqual(type(self.base.id), str, [])
-        self.assertEqual(type(self.base.created_at), datetime.datetime)
-        self.assertEqual(type(self.base.updated_at), datetime.datetime)
-
-    def test_initt(self):
-        """tests init"""
-        self.assertTrue(isinstance(self.base, BaseModel))
-
-    def test_save(self):
-        """tests save"""
-        self.base.save()
-        self.assertNotEqual(self.base.created_at, self.base.updated_at)
-
-
+    def test_BaseModel_save(self):
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.my_number = 89
+        storage = FileStorage()
+        storage.all().clear()
+        my_dict = my_model.to_dict()
+        sve = BaseModel()
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+        sve.save()
+        strmodel = f"[{my_model.__class__.__name__}] ({my_model.id}) {my_model.__dict__}"
+        self.assertEqual(my_model.__class__.__name__, "BaseModel")
+        self.assertEqual(my_model.name, "My First Model")
+        self.assertEqual(my_model.my_number, 89)
+        self.assertTrue(isinstance(my_model.created_at, datetime))
+        self.assertTrue(isinstance(my_model.updated_at, datetime))
+        self.assertEqual(my_dict["id"], my_model.id)
+        self.assertEqual(strmodel, my_model.__str__())
+        self.assertTrue(os.path.exists("file.json"))
 
 if __name__ == '__main__':
     unittest.main()
