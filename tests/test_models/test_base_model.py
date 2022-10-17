e2ff0a1c54dfc4b8b32d47995bf8ec4694bdf923
Denisse Landau <denisselandau97@gmail.com>
2022-10-16 22:08:55 -0300

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -10,22 +10,20 @@ import datetime
 class Base_Model_Tests(unittest.TestCase):
     """test attrubute """
 
+    def setUp(self):
+        base0 = BaseModel()
     def test_attributes(self):
         """check attributes"""
-        base0 = BaseModel()
-
         self.assertEqual(type(base0.id), str, [])
         self.assertEqual(type(base0.created_at), datetime.datetime)
         self.assertEqual(type(base0.updated_at), datetime.datetime)
 
     def test_initt(self):
         """tests init"""
-        base0 = BaseModel()
         self.assertTrue(isinstance(self.base0, BaseModel))
 
     def test_save(self):
         """tests save"""
-        base0 = BaseModel()
         self.base0.save()
         self.assertNotEqual(self.base0.created_at, self.base0.updated_at)
 
